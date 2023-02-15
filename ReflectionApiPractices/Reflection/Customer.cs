8c8f9aa33e87d3156699f3301768ef92a989ad9b
mostofa kamal <mostofakamal1990@gmail.com>
2023-02-15 08:49:38 +0600

    Reflection Apin

---
+++ ReflectionApiPractices/Reflection/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionApiPractices.Reflection
+{
+    public class Customer
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<string> ListOfPrperty { get; set; }
+
+        public Customer(int id , string name) {
+            Id = id;
+            Name = name;   
+        }
+
+        public void PrintId()
+        {
+
+            Console.WriteLine("Id No {0}", Id);
+        }
+
+
+        public void PrintName()
+        {
+
+            Console.WriteLine("Name : {0}", Name);
+        }
+
+
+    }
+}
---
+++ ReflectionApiPractices/Reflection/ReflectionApply.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionApiPractices.Reflection
+{
+    public class ReflectionApply
+    {
+        public void ReplactionImplentions()
+        {
+
+
+            Type t = Type.GetType("ReflectionApiPractices.Reflection.Customer");
+
+            Console.WriteLine("Class Name with NameSpace = "+t.FullName + ",  Class or Object name = "+ t.Name);
+            Console.WriteLine("NameSpace = "+t.Namespace);
+            Console.WriteLine("Relactions Typs = "+t.ReflectedType);
+            Console.WriteLine("Base type = "+t.BaseType);
+            Console.WriteLine("Attributes = "+t.Attributes);
+            Console.WriteLine("Methods = "+t.MemberType);
+            Console.WriteLine("Properties = "+t.GetProperties());
+            Console.WriteLine("Fields = "+t.GetFields());
+            Console.WriteLine("Custom Attributes = "+t.CustomAttributes);
+            Console.WriteLine("Get Member = "+t.GetMember("Customer"));
+
+            Console.WriteLine("======================= Get Properties ==================");
+
+            foreach(PropertyInfo p in t.GetProperties())
+            {
+
+                Console.WriteLine("property Name = "+ p.Name + " , Property Type = "+ p.PropertyType);
+            }
+            Console.WriteLine("=========================== Get members ========================");
+            foreach (MemberInfo m in t.GetMembers())
+            {
+                Console.WriteLine("Member Name = "+m.Name + ",  Reflection Type ="+  m.ReflectedType);
+            }
+
+            Console.WriteLine("=================== get Methods =====================");
+
+            MethodInfo[] methodInfos = t.GetMethods();
+                
+
+            foreach(MethodInfo m in methodInfos) {
+                Console.WriteLine("Method Name = "+ m.Name + ", Method Return Type "+m.ReturnType);
+                    
+                    
+                    }
+        }
+    }
+}
