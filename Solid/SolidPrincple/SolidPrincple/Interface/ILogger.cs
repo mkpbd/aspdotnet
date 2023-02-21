8ca7766a15ec93a98c980a2952f7df5fa0db5ecb
mkpbd <mostofakamal1990@gmail.com>
2023-02-21 12:40:46 +0600

    Single responsibility principle

---
+++ Solid/SolidPrincple/SolidPrincple/Interface/ILogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrincple.Interface
+{
+    public interface ILogger
+    {
+        void Info(string info);
+        void Debug(string info);
+        void Error(string message, Exception ex);
+    }
+}
