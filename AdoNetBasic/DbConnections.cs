39461a5a932557fea8eb60c138ffab53f6cb830d
mostofa kamal <mostofakamal1990@gmail.com>
2023-04-03 04:20:57 +0600

    Ado dot net practices

---
+++ AdoNetBasic/DbConnections.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetBasic
+{
+    public class DbConnections
+    {
+
+        public static SqlConnection Connection()
+        {
+            string connectionString = $"data source=SERVER\\MSSQLSERVER01; database=student; integrated security=SSPI";
+
+            SqlConnection sqlConnection = new SqlConnection(connectionString);  
+            sqlConnection.Open();
+            return sqlConnection;
+        }
+    }
+}
