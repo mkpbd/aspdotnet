899211dc9d9dcf75c746c26638e61c67e833bbd8
mostofa kamal <mostofakamal1990@gmail.com>
2023-04-04 20:29:04 +0600

    Linq data From Database

---
+++ AdoNetBasic/Models/AccessTypedDS.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetBasic.Models
+{
+    public class AccessTypedDS
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public string TypeDescription { get; set; }
+        
+
+    }
+}
