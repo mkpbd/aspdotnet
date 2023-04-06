07dd18004ab4371a28bb2cf6b9fa432ed694f6d7
mkpbd <mostofakamal1990@gmail.com>
2023-04-06 10:37:23 +0600

    AdoDotnetBasicStart

---
+++ AdoDotnetTutorials/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdoDotnetTutorials
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
