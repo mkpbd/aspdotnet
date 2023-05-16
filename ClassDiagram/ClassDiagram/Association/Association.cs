013b585e27c8ec93d44ad1c6b8ce45420f970d94
mkpbd <mostofakamal1990@gmail.com>
2023-05-16 16:53:33 +0600

    Add Class diagram Add

--- ClassDiagram/ClassDiagram/Association/Association.cs
+++ ClassDiagram/ClassDiagram/Association/Association.cs
@@ -1,12 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ClassDiagram.Association
 {
-    internal class Association
+    public class Student
     {
+        public int StudentId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+    }
+
+    public class StudentRepository
+    {
+        public Student GetStudent(int StudentId)
+        {
+            // get student by id from db here
+
+            return new Student();
+        }
+
+        public bool Save(Student student)
+        {
+            // save student to db here
+            Console.WriteLine("Student saved successfully");
+
+            return true;
+        }
+
+        public bool Validate(Student student)
+        {
+            // get student from db to check whether the data is already exist
+            Console.WriteLine("Student does not exist.");
+
+            return true;
+        }
     }
 }
