a9a676075e79eace78003bb8724d41ffedacdbad
mkpbd <mostofakamal1990@gmail.com>
2023-05-22 14:15:56 +0600

    Modified

--- src/CleanArchitecture/Domain/Common/BaseAuditableEntity.cs
+++ src/CleanArchitecture/Domain/Common/BaseAuditableEntity.cs
@@ -1,6 +1,6 @@
 ﻿namespace Domain.Common
 {
-    public abstract class BaseAuditableEntity
+    public abstract class BaseAuditableEntity : BaseEntity
     {
         public DateTime Created { get; set; }
 
