4d9e3f9c1ee7a6194d7304d3125f324f9024a652
mkpbd <mostofakamal1990@gmail.com>
2023-05-22 14:37:29 +0600

    Record

---
+++ DDDEDDTDD/Records/DateJoke.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Records
+{
+
+    /****
+     * 
+     * 
+     * A record in C# is a new data type that was introduced in C# 9. It is a lightweight, immutable data type that is similar to a class, but with some key differences. Records are thread-safe, and because they are immutable, you cannot change them after they are created. Records also have built-in support for equality and formatting.
+     * 
+     */
+    public record DateJoke
+    {
+        public string Question { get; init; }
+        public string Answer { get; init; }
+    }
+
+
+    public record Joke
+    {
+        public string Setup { get; init; }
+        public string Punchline { get; init; }
+    }
+
+    /***
+     * This record represents a person with a name and an age. It is humble because it does not have any special properties or methods. It is simply a data structure that can be used to store information about a person.
+     */
+
+    public record Peson(string Name, int Age, string Address);
+
+
+    /**
+     * 
+     * This record represents a book with a title, an author, and a number of pages. It is humble because it does not have any special properties or methods. It is simply a data structure that can be used to store information about a book.
+     ***/
+
+    public record Book(string Title, string Author, int Pages);
+}
---
+++ DDDEDDTDD/Records/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Records
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            DateJoke dateJoke = new DateJoke
+            {
+                Question = "What is your Question Now",
+                Answer = "I can not give any anweser "
+            };
+
+
+            var joke = new Joke()
+            {
+                Punchline = "There is no punch  line here",
+                Setup = "this is a new setup run hear "
+            };
+
+
+
+              new  Peson("mostofa kamal", 32, "Gazipur");
+
+
+
+
+        }
+
+
+        public void TestMethod()
+        {
+
+          
+        }
+
+         
+    }
+}
