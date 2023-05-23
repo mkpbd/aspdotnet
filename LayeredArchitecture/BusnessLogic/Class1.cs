b5d8f745861a759b7bd6970b137f944e09dd001d
mostofa kamal <mostofakamal1990@gmail.com>
2023-05-23 06:15:08 +0600

    layer Architecture

---
+++ LayeredArchitecture/BusnessLogic/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace BusnessLogic
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ LayeredArchitecture/CLIPresentaionLayer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CLIPresentaionLayer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
