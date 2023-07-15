47f1f9d0a3a8dfde775bbb1895f330251d2fbbae
mkpbd <mostofakamal1990@gmail.com>
2023-07-15 15:06:44 +0600

    Domin Model Design

---
+++ eShop/Domain/Orders/Order.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Orders
+{
+    public class Order
+    {
+        private readonly HashSet<LineItem> _itemLists = new HashSet<LineItem>();
+        public Guid Id { get; private set; }
+        public Guid CustomerId { get; private set; }
+        public void Add(Product product)
+        {
+            var LineItem = new LineItem(Guid.NewGuid(),Id, product.Id,product.Price);
+            _itemLists.Add(LineItem);
+        }
+
+    }
+}
