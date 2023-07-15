dfbc385a8beb430062c33e7b1029e9c1cb2d99ff
mkpbd <mostofakamal1990@gmail.com>
2023-07-15 18:01:53 +0600

    Persistence Layer Created and configuration levels

---
+++ eShop/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer();
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ eShop/Persistence/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Customers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Configurations
+{
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+    
+
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+           builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).HasConversion(customerId => customerId.Value, Value => new CustomerId(Value));
+            builder.Property(x => x.Name).HasMaxLength(100);
+            builder.Property(x => x.Email).HasMaxLength(100);
+            builder.Property(x => x.Email).IsUnicode(true);
+        }
+    }
+}
