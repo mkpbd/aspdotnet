2e7b4be952b71da433b8a415788acb505a798241
mostofa kamal <mostofakamal1990@gmail.com>
2023-07-16 22:25:16 +0600

    Add Category

--- DomainDrevenDesign/Domain/WithoutDdd/ProductCategory.cs
+++ DomainDrevenDesign/Domain/WithoutDdd/ProductCategory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Catalog.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,12 @@ namespace Domain.WithoutDdd
     public class ProductCategory
     {
         public int Id { get; set; }
-        public string? CategoryName { get; set; }
+       public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+        public int ProductId { get; set; }
+        public Product? Product { get; set; }
+
+
 
     }
 }
