2f62e148f23a7be3169d0fd98d92f6528150b6fe
mkpbd <mostofakamal1990@gmail.com>
2023-07-17 12:43:47 +0600

    Basic identity frameWorkConfiguration

---
+++ IdentityFaremwork/IdentityFaremwork/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using IdentityFaremwork.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityFaremwork.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> applicationUsers { get; set; }
+    }
+}
